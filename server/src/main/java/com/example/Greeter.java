9e2af2c4985492ea022c18dac5d0eec2509b419e
Silvano Gil <sgil@pronoide.es>
2019-06-12 10:51:12 +0200

    Actualización para que falle el findbugs

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -16,6 +16,7 @@ public class Greeter {
    * Metodo de greet modificado.
    */
   public String greet(String someone) {
+    Long lalala = new Long("2333");
     if(true){
       return String.format("Hello, %s!", someone);
     }
