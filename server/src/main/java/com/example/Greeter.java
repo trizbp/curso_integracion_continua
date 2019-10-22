070ab724a3b29256a7e767be1f6eaf22349c7240
Silvano Gil <sgil@pronoide.es>
2019-10-22 12:47:25 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -20,28 +20,29 @@ public class Greeter {
   public String greet(String someone) {
     Long lalala = new Long("2333");
     if(MAGICO > 1){
-      return String.format("Hello, %s!", someone);
+      if(true){
+     lalala = new Long("45678");
+      
+    if(true){
+     lalala = new Long("45678");
+    }
+      
+    if(true){
+     lalala = new Long("45678");
+    }
+    }
+    
+    if(true){
+     lalala = new Long("45678");
+    }
+    
+    if(true){
+     lalala = new Long("45678");
+    }
+            return String.format("Hello, %s!", someone);
     }else{
         return String.format("Hello, %s!", someone);
     }
-    if(true){
-     lalala = new Long("45678");
-      
-    if(true){
-     lalala = new Long("45678");
-    }
-      
-    if(true){
-     lalala = new Long("45678");
-    }
-    }
     
-    if(true){
-     lalala = new Long("45678");
-    }
-    
-    if(true){
-     lalala = new Long("45678");
-    }
   }
 }
